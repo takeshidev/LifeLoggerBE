41f76869888432573d07d1d52e510c5ce60dfca7
Takeshi Kita <dev.takeshi@gmail.com>
2023-01-14 20:55:19 -0300

    introducing MySQL

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
+const port = 3000;
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const db = require("./utils/database");
 const app = express();
 
 const feedRoutes = require("./routes/feed");
@@ -9,5 +10,5 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/feed", feedRoutes);
 
-console.log("Life logger backend server running...");
-app.listen(3000);
+console.log(`Life logger backend server running in port ${port}...`);
+app.listen(port);
